47688c98f9cf22e0e1fcbe0502f9462381b5b49e
studeri3819 <43508140+studeri3819@users.noreply.github.com>
2018-09-22 22:30:51 -0400

    Add files via upload
    First submission

---
+++ P2T1_SalesPrediction_IvanStuder.py
@@ -0,0 +1,14 @@
+# Prediciting sales for a customer
+# 9/18/2018
+# CTI-110 P2T1 - Sales Prediction
+# Ivan Studer
+
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+#Calculate the profit as 23 precent of total sales.
+profit = total_sales * 0.23
+
+#Display the profit.
+print('The profit is $', format(profit, ',.2f'))
+
